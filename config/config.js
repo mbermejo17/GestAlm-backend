529bcf18ca9df810d391f8710a0f55480066600c
mbermejo17 <mbermejo17@gmail.com>
2020-01-20 15:05:24 +0100

    sddsds

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ var settings = { "SEED":'este-es-un-seed-dificil',
 // Google
 "GOOGLE_CLIENT_ID" :'442737206823-dilej5tevnrv61sovd7bocf5qeafmjs3.apps.googleusercontent.com',
 "GOOGLE_SECRET" :'6cWrrFy8KchLLrYmg70esp7t',
-"PORT": 3000
+"PORT": 25000
 };
 
 module.exports = settings;
